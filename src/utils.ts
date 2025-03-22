9edc0bddc25ac5da33eea59cd9d4b79790d92bf9
Skas Merkushin <skas.merkushin@gmail.com>
2025-03-22 14:55:10 +0100

    chore: prettify response

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,22 @@
 import type { CodeResponse } from 'src/types';
+import type { Execution } from '@e2b/code-interpreter';
 
-export const parseCodeResposnse = (response: CodeResponse) => {
-    const stdout = response.logs.stdout[0];
-    const stderr = response.logs.stderr[0];
+export const parseCodeResposnse = (response: Execution | string[] | string | undefined) => {
+    if (!response) {
+        return { stdout: '', stderr: '', results: [], error: null };
+    }
+
+    if (typeof response === 'string') {
+        return { stdout: '', stderr: '', results: [response], error: null };
+    }
+
+    if (Array.isArray(response)) {
+        return { stdout: '', stderr: '', results: response, error: null };
+    }
+
+    const stdout = response.logs.stdout?.[0];
+    const stderr = response.logs.stderr?.[0];
     const results = response.results;
-    return { stdout, stderr, results };
+    const error = response.error;
+    return { stdout, stderr, results, error };
 }
